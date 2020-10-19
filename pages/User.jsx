4d18691619ebf9f02dad8de4b282e16f5de23c6a
Nicolas Guzman <deathisue@gmail.com>
2020-10-19 14:01:32 -0400

    User stuff
    esto es lo poco que he hecho pero aprendi mucho down the line.

---
+++ pages/User.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useMutation, gql } from "@apollo/client";
+import { useRouter } from "next/router";
+import { UserWrapper } from "../containers/UserWrapper";
+function TestUser() { }
+export default function User() {
+    return (
+        <>
+            <UserWrapper />
+        </>
+    );
+}
